7990dde1ddcd7200f50576e929af285b290f69f0
Matthew <140831930+matthew110703@users.noreply.github.com>
2025-04-12 00:26:02 +0530

    Added new script command to generate sitemap automatically

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://mathew-pi.vercel.app/",
+  siteUrl: "https://mathew-pi.vercel.app",
   generateRobotsTxt: true,
+  sitemapSize: 7000,
 };
